b87cdecb0634dc9c62f08ac67c20e5d858e3c591
André L. C. Moreira <alcm99@gmail.com>
2020-06-21 15:34:44 -0300

    implementation of /devices endpoint

--- README.md
+++ README.md
@@ -3,8 +3,56 @@
 ### 1. Brief Introduction
 
 This document aims to describe the usage of all resources provided by API, giving all information
-related to each defined endpoint (requirements, available operations and expected results).
+related to each defined endpoint (arguments, available operations and expected results).
 
 ### 2. Endpoints
 
-TODO
+#### 2.1 Devices
+
+**Get all devices**
+
+`GET /devices`
+
+**Arguments**
+
+- ```user```: The user name registered on server
+- ```pass```: The password associated to the user
+
+**Expected response**
+
+- **200** on success
+
+```json
+[
+    {
+        "id": "foo",
+        "description": "device description",
+        "status": "online",
+        "group": "group-foo",
+        "coordinates": "-3.71839,-38.5434"
+    },
+    {
+        "id": "bar",
+        "description": "device description",
+        "status": "offline",
+        "group": "group-bar",
+        "coordinates": "-4.83093,-37.7816"
+    }
+]
+```
+
+Or when the database is empty:
+
+```json
+{
+    "message": "no devices registered!"
+}
+```
+
+- **500** on failure
+
+```json
+{
+    "message": "internal server error!"
+}
+```
---
+++ scripts/init_db.js
@@ -0,0 +1,46 @@
+db_name = 'delator-db'
+db = db.getSiblingDB(db_name)
+
+db.createRole(
+    {
+        role: 'device-role',
+        privileges : [
+            { resource : { 'db' : db_name, 'collection': 'ocurrences' }, actions: ['insert'] },
+            { resource : { 'db' : db_name, 'collection': 'devices' }, actions: ['insert'] }
+        ],
+        roles : []
+    }
+);
+
+db.createRole(
+    {
+        role: 'front-role',
+        privileges : [
+            { resource : { 'db' : db_name, 'collection': 'ocurrences' }, actions: ['find'] },
+            { resource : { 'db' : db_name, 'collection': 'devices' }, actions: ['insert', 'remove', 'find'] }
+        ],
+        roles : []
+    }
+);
+
+db.createUser(
+    {
+        user: 'front-user',
+        pwd: 'front-pass',
+        roles: [
+            { role: 'front-role', db: db_name, collection: 'ocurrences' },
+            { role: 'front-role', db: db_name, collection: 'devices' }
+        ]
+    }
+);
+
+db.createUser(
+    {
+        user: 'device-user',
+        pwd: 'device-pass',
+        roles: [
+            { role: 'device-role', db: db_name, collection: 'ocurrences' },
+            { role: 'device-role', db: db_name, collection: 'devices' }
+        ]
+    }
+);
