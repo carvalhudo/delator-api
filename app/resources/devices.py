79474793da964c2cb0e3d266869df3e250d4ec4a
André L. C. Moreira <alcm99@gmail.com>
2020-07-11 19:27:49 -0300

    move the implementation of POST /device/id to /devices endpoint

--- app/resources/devices.py
+++ app/resources/devices.py
@@ -3,6 +3,7 @@ from flask_restful.reqparse import RequestParser
 
 from models.device_model import DeviceModel
 from errors.exceptions.resource_does_not_exist import ResourceDoesNotExist
+from errors.exceptions.resource_already_exist import ResourceAlreadyExist
 
 class Devices(Resource):
 
@@ -37,3 +38,34 @@ class Devices(Resource):
             raise ResourceDoesNotExist
 
         return data, 200
+
+    def post(self):
+        """
+        POST /devices implementation
+
+        :returns: If the device was successfully registered, a success message
+                  with the associated code; otherwise the suitable error message
+
+        """
+        self.__parser.add_argument('device-id', type=str, required=True)
+        self.__parser.add_argument('serial-number', type=str, required=True)
+        self.__parser.add_argument('description', type=str, required=True)
+        self.__parser.add_argument('group', type=str, required=True)
+
+        args = self.__parser.parse_args()
+
+        # request parameters
+        user = args['user']
+        passwd = args['pass']
+        device_id = args['device-id']
+        serial_number = args['serial-number']
+        description = args['description']
+        group = args['group']
+
+        model = DeviceModel(user, passwd)
+        if model.get(device_id):
+            raise ResourceAlreadyExist
+
+        model.insert(device_id, serial_number, description, group)
+
+        return {'message': 'resource created!'}, 201
